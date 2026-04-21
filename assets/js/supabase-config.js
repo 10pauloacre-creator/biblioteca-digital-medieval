0a2a7b3651f864c0c934f3be62246d546d63b7be
10pauloacre-creator <10pauloacre@gmail.com>
2026-04-20 20:21:00 -0500

    S

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -5,3 +5,4 @@
 window.SUPABASE_URL  = 'https://vgceathgwvtmjxbdpecr.supabase.co';
 window.SUPABASE_ANON = 'sb_publishable_ba-g-ww4KwM2Wq0x2vsGVg_x_9pTqUQ';
 window.ADMIN_EMAIL   = '10pauloacre@gmail.com';
+window.ADMIN_PASSWD  = '9P9R6R4m@31$#';
